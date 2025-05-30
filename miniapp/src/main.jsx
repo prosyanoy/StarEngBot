085dac6389b06aa1ec6f9417728b2d9f15deb532
Ilia Prosianoi <isprosianoi@edu.hse.ru>
2025-05-30 22:12:07 +0300

    main

--- miniapp/src/main.jsx
+++ miniapp/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx';
 import './i18n.js';
 import './index.css';
 import { AuthProvider } from '@/lib/api';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <BrowserRouter basename="/stareng">
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
